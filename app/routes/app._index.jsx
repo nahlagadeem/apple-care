d43803d4f285afbebf1bf539b0141d618d3577d5
nahlagadeem <nahla.gadeem@gmail.com>
2026-06-30 15:20:33 +0300

    Reduce admin home copy

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -9,26 +9,8 @@ export const loader = async ({ request }) => {
 export default function AppHome() {
   return (
     <s-page heading="Apple Care">
-      <s-section heading="AppleCare+ add-on service">
-        <s-stack gap="base">
-          <s-box>
-            <s-heading>Available on the storefront</s-heading>
-            <s-paragraph>
-              AppleCare+ is now available for eligible products on the storefront. Customers can choose AppleCare+
-              before adding a product to cart.
-            </s-paragraph>
-          </s-box>
-
-          <s-box>
-            <s-heading>Where it appears</s-heading>
-            <s-unordered-list>
-              <s-list-item>Product pages</s-list-item>
-              <s-list-item>Quick-view product popups</s-list-item>
-              <s-list-item>Eligible bundle products</s-list-item>
-              <s-list-item>Cart and checkout after the customer selects AppleCare+</s-list-item>
-            </s-unordered-list>
-          </s-box>
-        </s-stack>
+      <s-section>
+        <s-paragraph>AppleCare+ is now available in the storefront.</s-paragraph>
       </s-section>
     </s-page>
   );
